37e0154f9602fb0344aceeac6193aa1aed2ae495
Danila.Vorontsov <silversun09@mail.ru>
2022-07-08 21:35:01 +1000

    Задача 25

--- Program.cs
+++ Program.cs
@@ -1,29 +1,23 @@
-﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
+﻿//Задача 25: 
+//Напишите цикл, который принимает на вход два числа (A и B) и воз
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
 
-Console.WriteLine("\n Задача 23");
-Console.Write("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
 
-void Cube(int[] cube){
-  int counter = 0;
-  int length = cube.Length;
-  while (counter <  length){
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-    counter++;
+int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
   }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
 }
 
-void PrintArry(int[] coll){
-  int count = coll.Length;
-  int index = 0;
-  while(index < count){
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
-} 
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
 
-int[] arry = new int[cube+1];
-Cube(arry);
-PrintArry(arry);
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
