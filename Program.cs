ad94e8ec16bd760f243ef599d20b2cdc1c3e03ef
Danila.Vorontsov <silversun09@mail.ru>
2022-07-12 14:28:55 +1000

    Задача 36

--- Program.cs
+++ Program.cs
@@ -1,37 +1,32 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
-Console.Clear();
-Console.WriteLine($"Задача 34. Количество чётных чисел в массиве:\n");
-int[] numbers = new int[10];
 
-void FillArray(int[] array, int min, int max){
-  for (int i = 0; i<array.Length; i++ ){
-    array[i] = new Random().Next(min, max);
-  }
-}
+// Console.Clear();
+Console.WriteLine($"\nЗадача 36. Сумма элементов, стоящих на нечётных позициях:\n");
+Console.Write($"Введи количество элементов массива: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
 
-void WriteArray(int[] array){
-    for (int i = 0; i<array.Length; i++ ){
-    Console.Write(array[i] + " ");
-  }
-  Console.WriteLine();
-}
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
 
-int QuantityPositive(int[] array){
-    int quantity = 0;
-    for (int i = 0; i<array.Length; i++ ){
-    if (array[i] % 2 == 1)
-    {
-      quantity++;
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
     }
+  return sumElements;
   }
-  return quantity;
-}
 
-FillArray(numbers, 100, 1000);
-WriteArray(numbers);
-Console.WriteLine();
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
 
-int quantity = QuantityPositive(numbers);
-Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
